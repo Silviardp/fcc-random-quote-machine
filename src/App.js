153cc76a5202fcbca3694cd283fa0e03dae2c813
Silvia Rocha <silviadprocha@gmail.com>
2020-11-08 15:17:25 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -11,11 +11,18 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ quotes }));
+  }
+
   nextQuoteClickHandler() {
     console.log('hi')
   }
 
   render() {
+    console.log(this.state.quotes);
     return (
       <div className="App" id="quote-box">
         <Button buttonDisplayName="Next Quote" clickHandler={this.nextQuoteClickHandler}/>
