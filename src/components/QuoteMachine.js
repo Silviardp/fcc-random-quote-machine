0aa87feb5e2755e7a90734a54b4b5d3aae412f84
Silvia Rocha <silviadprocha@gmail.com>
2020-11-10 22:19:54 +0100

    added fortawesome libraries and twitter icon

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesome, FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const QuoteMachine = (props) => (
   <Card>
@@ -19,6 +22,12 @@ const QuoteMachine = (props) => (
       </CardContent>
       <CardActions>
         <Button variant="outlined" color="secondary" size="small" onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+        <IconButton
+          target="_blank"
+          href={`https://twitter.com/intent/tweet?text=${props.selectedQuote.quote}&hashtags=sdpr90`}
+        >
+          <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+        </IconButton>
       </CardActions>
   </Card>
 );
