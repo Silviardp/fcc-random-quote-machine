b246151652c5f5204b46340189e0a7d0669bb166
Silvia Rocha <silviadprocha@gmail.com>
2020-11-11 22:37:30 +0100

    clean code

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -5,7 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import { FontAwesome, FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const QuoteMachine = (props) => (
